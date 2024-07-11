846eb29a2d3e293f6ddbdc2f035fa98ee3755a1a
reality_user <jc3124567@gmail.com>
2024-07-11 12:25:07 -0500

    progressing on parser tutorial

---
+++ parser2/target/debug/deps/parser2-676e17ec0f91d43f.d
@@ -0,0 +1,6 @@
+/home/jesse/Documents/prog/c++/compiler4/parser2/target/debug/deps/parser2-676e17ec0f91d43f: src/main.rs src/parser.rs
+
+/home/jesse/Documents/prog/c++/compiler4/parser2/target/debug/deps/parser2-676e17ec0f91d43f.d: src/main.rs src/parser.rs
+
+src/main.rs:
+src/parser.rs:
---
+++ parser2/target/debug/deps/parser2-c723f1ffa4f04b9b.d
@@ -0,0 +1,6 @@
+/home/jesse/Documents/prog/c++/compiler4/parser2/target/debug/deps/parser2-c723f1ffa4f04b9b: src/main.rs src/parser.rs
+
+/home/jesse/Documents/prog/c++/compiler4/parser2/target/debug/deps/parser2-c723f1ffa4f04b9b.d: src/main.rs src/parser.rs
+
+src/main.rs:
+src/parser.rs:
