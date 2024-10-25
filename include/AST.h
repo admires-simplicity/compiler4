c743fa8a5ce833e5a9bf7eb6a090f307c523e195
reality_user <jc3124567@gmail.com>
2024-10-25 15:31:16 -0500

    deleted unused comments in AST.h and AST.cpp

--- include/AST.h
+++ include/AST.h
@@ -10,123 +10,6 @@
 
 class SyntaxNodeVisitor;
 
-// class SyntaxNode {
-// public:
-//   enum class NodeType {
-//     literal,
-//     identifier,
-//     value,
-//     fn_def,
-//     apply,
-//     block,
-//     program_block,
-
-//     arg_list,
-
-//     add,
-//   };
-
-//   static Bimap<NodeType, std::string> NodeType_repr;
-
-//   std::unordered_map<SyntaxNode::NodeType, Type>& get_NodeType_to_Type_map();
-//   Type NodeType_to_Type(NodeType type);
-
-
-
-//   NodeType type = NodeType::literal;
-//   Token *token;
-//   Type val_type; // int -- value, TypeIdList -- function // TODO: think about how to actually typecheck Types
-//   std::vector<SyntaxNode *> children;
-
-//   SyntaxNode(std::string string) : token(new Token(string)) {}
-//   SyntaxNode(std::string string, std::vector<SyntaxNode *> children) : token(new Token(string)), children(children) {}
-  
-//   SyntaxNode(Token *token) : token(token) {}
-//   SyntaxNode(Token *token, std::vector<SyntaxNode *> children) : token(token), children(children) {}
-//   //SyntaxNode(Token *token, Type type) : token(token), val_type(type) {}
-
-//   SyntaxNode(NodeType type) : type(type), val_type(NodeType_to_Type(type)) {}
-//   SyntaxNode(NodeType type, std::vector<SyntaxNode *> children) : type(type), children(children), val_type(NodeType_to_Type(type)) {}
-
-//   SyntaxNode(Type type) : val_type(type) {}
-
-//   ~SyntaxNode() {
-//     delete token;
-//     for (auto child : children) {
-//       delete child;
-//     }
-//   }
-
-//   std::string type_name() {
-//     return NodeType_repr[type];
-//   }
-
-//   void set_type();
-
-//   /*
-//    * max_inline_depth -- maximum depth of a node which for its children to be
-//    *                     printed inline. if max_inline_depth = 1, only childless
-//    *                     nodes will be printed inline.
-//    */
-//   std::string to_string(bool pretty = false, bool display_type = false,  int max_inline_depth = 1, int indent = 0) {
-//     std::string s = "";
-//     if (pretty && indent) {
-//       s += "\n";
-//       for (int i = 0; i < indent; i++) s += "  ";
-//     }
-
-//     if (children.size() == 0) {
-//       s += node_signature(display_type);
-//       return s;
-//     }
-    
-//     s += "(";
-//     s += node_signature(display_type);
-
-//     int md = (pretty) ? max_depth(this) : 0; // max depth is slow.
-//     // what would be better is to memoize the depth of every node whenever
-//     // to_string is called by an external function.
-
-//     for (auto child : children) {
-//       s += " ";
-//       s += (pretty && md > max_inline_depth) ? child->to_string(pretty, display_type, max_inline_depth, indent+1) : child->to_string(pretty, display_type);
-//     }
-//     s += ")";
-//     return s;
-//   }
-
-// private:
-//   int max_depth(SyntaxNode *node) {
-//     if (node->children.size() == 0) return 0;
-//     int max = 0;
-//     for (auto child : node->children) {
-//       int depth = max_depth(child);
-//       if (depth > max) max = depth;
-//     }
-//     return max + 1;
-//   }
-
-//   bool has_type() { // TODO: sort of hacky
-//     switch (type) {
-//       case NodeType::literal:
-//       case NodeType::fn_def:
-//         return true;
-//       default:
-//         return false;
-//     }
-//   }
-
-//   std::string node_signature(bool display_type = false) {
-//     return
-//       ((display_type) ? (type_print_repr(val_type) + ":") : "")
-//         +
-//       ((type == NodeType::literal) ? token->literal : NodeType_repr[type]);
-//   }
-
-// };
-
-
-
 class SyntaxNode {
 public:
   virtual void accept(SyntaxNodeVisitor &v) = 0;
@@ -147,36 +30,9 @@ public:
   std::string to_string() override;
 };
 
-// class IdentNode : public ValueNode {
-// public:
-//   IdentNode(Type type, Token *token) : ValueNode(type, token) {}
-//   IdentNode(Token *token) : ValueNode(TypeSet::get_id("dynamic"), token) {}
-// };
-
-// class ArgListNode : public SyntaxNode {
-// public:
-//   std::vector<SyntaxNode *> args;
-//   ArgListNode(std::vector<SyntaxNode *> args) : args(args) {}
-
-//   std::string to_string() override {
-//     std::string s;
-//     for (auto arg : args) {
-//       s += arg->to_string() + " ";
-//     }
-//     if (s.size()) s.pop_back();
-//     return s;
-//   }
-
-//   SyntaxNode *operator[](int i) {
-//     return args[i];
-//   }
-// };
-
 class ApplyNode : public SyntaxNode {
 public:
   SyntaxNode *fn;
-  //ArgListNode *args;
-  //ApplyNode(SyntaxNode *fn, ArgListNode *args) : fn(fn), args(args) {}
   std::vector<SyntaxNode *> args;
   ApplyNode(SyntaxNode *fn, std::vector<SyntaxNode *> args) : fn(fn), args(args) {}
 
@@ -217,11 +73,9 @@ public:
 class FnDefNode : public SyntaxNode {
 public:
   ValueNode *ident;
-  //Type type = TypeSet::get_id("dynamic");
   std::vector<SyntaxNode*> args;
   BlockNode *block;
   FnDefNode(ValueNode *ident, std::vector<SyntaxNode*> args, BlockNode *block) : ident(ident), args(args), block(block) {}
-  //FnDefNode(ValueNode *ident, Type type, std::vector<SyntaxNode*> args, BlockNode *block) : ident(ident), type(type), args(args), block(block) {}
 
   void accept(SyntaxNodeVisitor &v) override;
   inline std::string name() override;
--- src/lib/AST.cpp
+++ src/lib/AST.cpp
@@ -9,37 +9,6 @@
 int depth(SyntaxNode *node);
 int indent = 0;
 
-// Bimap<SyntaxNode::NodeType, std::string> SyntaxNode::NodeType_repr = {
-//   {NodeType::literal, "literal"},
-//   {NodeType::identifier, "identifier"},
-//   {NodeType::value, "value"},
-//   {NodeType::fn_def, "fn_def"},
-//   {NodeType::apply, "apply"},
-//   {NodeType::block, "block"}, 
-//   {NodeType::program_block, "program_block"},
-
-//   {NodeType::arg_list, "arg_list"},
-
-//   {NodeType::add, "+"},
-// };
-
-// std::unordered_map<SyntaxNode::NodeType, Type>& SyntaxNode::get_NodeType_to_Type_map() {
-//   static std::unordered_map<SyntaxNode::NodeType, Type> NodeType_to_Type {
-//     {SyntaxNode::NodeType::add, TypeIdList{"num", "num", "num"}},
-//   };
-//   return NodeType_to_Type;
-// }
-
-// Type SyntaxNode::NodeType_to_Type(SyntaxNode::NodeType type) {
-//   return get_NodeType_to_Type_map()[type];
-// }
-
-// void SyntaxNode::set_type() {
-//   if (type != NodeType::literal) {
-//     val_type = NodeType_to_Type(type);
-//   }
-// }
-
 void ValueNode::accept(SyntaxNodeVisitor &v) {
   v.visit(this);
 }
